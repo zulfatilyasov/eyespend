de2f512cf970ca92aa2f3a9c9fd589cedf82c9aa
Zulfat <zulfatkin@gmail.com>
2014-04-30 18:59:03 +0400

    grunt file dev and prod options

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,79 @@
 module.exports = function (grunt) {
+    var scripts = [
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angular-route/angular-route.js',
+            'bower_components/angular-sanitize/angular-sanitize.js',
+
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/toastr/toastr.js',
+            'app/lib/select2.js',
+            'app/lib/modernizr-custom.js',
+            'app/lib/sticky/sticky.js',
+            'bower_components/datetimepicker/jquery.datetimepicker.js',
+            'bower_components/mockJSON/js/jquery.mockjson.js',
+            'bower_components/ng-tags-input/ng-tags-input.js',
+            'bower_components/ngAutocomplete/src/ngAutocomplete.js',
+            'bower_components/angular-i18n/angular-locale_ru-ru.js',
+            'app/lib/sticky/sticky.js',
+            'bower_components/RandomColor/rcolor.js',
+            'bower_components/AngularGM/angular-gm.js',
+
+            'app/app.js',
+            'app/config.exceptionHandler.js',
+            'app/config.js',
+            'app/config.route.js',
+            'app/services/datacontext.js',
+            'app/services/directives.js',
+            'app/services/constants.js',
+            'app/services/date.js',
+            'app/services/transaxns.js',
+            'app/common/common.js',
+            'app/common/logger.js',
+            'app/transactions/transactions.js',
+            'app/shell/shell.js',
+
+            'app/services/modelStub.js',
+            'app/httpBackendStub.js'
+        ],
+        styles = [
+            'app/css/application.css',
+            'bower_components/toastr/toastr.css',
+            'bower_components/ng-tags-input/ng-tags-input.css',
+            'bower_components/datetimepicker/jquery.datetimepicker.css',
+            'app/css/app.css',
+            'app/css/overlay.css',
+            'app/lib/sticky/sticky.css'
+        ],
+        uglifyOptions = {
+            mangle: false,
+            compress: false
+        },
+        uglifyDevOptions= {
+            mangle: false,
+            compress: false,
+            sourceMap : true
+        };
 
     grunt.initConfig({
         uglify: {
-            options: {
-                mangle: false,
-                compress:false
-            },
-            my_target: {
+            dev: {
+                options: uglifyDevOptions,
                 files: {
-                    'app/eyeSpend.min.js': [
-                        'bower_components/angular/angular.js',
-                        'bower_components/angular-mocks/angular-mocks.js',
-                        'bower_components/angular-route/angular-route.js',
-                        'bower_components/angular-sanitize/angular-sanitize.js',
-
-                        'bower_components/jquery/dist/jquery.js',
-                        'bower_components/toastr/toastr.js',
-                        'app/lib/select2.js',
-                        'app/lib/modernizr-custom.js',
-                        'app/lib/sticky/sticky.js',
-                        'bower_components/datetimepicker/jquery.datetimepicker.js',
-                        'bower_components/mockJSON/js/jquery.mockjson.js',
-                        'bower_components/ng-tags-input/ng-tags-input.js',
-                        'bower_components/ngAutocomplete/src/ngAutocomplete.js',
-                        'bower_components/angular-i18n/angular-locale_ru-ru.js',
-                        'app/lib/sticky/sticky.js',
-                        'bower_components/RandomColor/rcolor.js',
-                        'bower_components/AngularGM/angular-gm.js',
-
-                        'app/app.js',
-                        'app/config.exceptionHandler.js',
-                        'app/config.js',
-                        'app/config.route.js',
-                        'app/services/datacontext.js',
-                        'app/services/directives.js',
-                        'app/services/constants.js',
-                        'app/services/date.js',
-                        'app/services/transaxns.js',
-                        'app/common/common.js',
-                        'app/common/logger.js',
-                        'app/transactions/transactions.js',
-                        'app/shell/shell.js',
-
-                        'app/services/modelStub.js',
-                        'app/httpBackendStub.js'
-                    ]
+                    'app/eyeSpend.min.js': scripts
+                }
+            },
+            prod: {
+                options: uglifyOptions,
+                files: {
+                    'app/eyeSpend.min.js': scripts
                 }
             }
         },
         cssmin: {
             combine: {
                 files: {
-                    'app/eyeSpend.min.css': [
-                        'app/css/application.min.css',
-                        'bower_components/toastr/toastr.css',
-                        'bower_components/ng-tags-input/ng-tags-input.css',
-                        'bower_components/datetimepicker/jquery.datetimepicker.css',
-                        'app/css/app.css',
-                        'app/css/overlay.css',
-                        'app/lib/sticky/sticky.css'
-                    ]
+                    'app/eyeSpend.min.css': styles
                 }
             }
         },
@@ -70,7 +84,7 @@ module.exports = function (grunt) {
                     '!app/eyeSpend.min.css',
                     '!app/eyeSpend.min.js'
                 ],
-                tasks: ['uglify', 'cssmin'],
+                tasks: ['default'],
                 options: {
                     interrupt: true,
                     livereload: true
@@ -83,5 +97,6 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    grunt.registerTask('default', ['uglify', 'cssmin']);
+    var target = grunt.option('target') || 'dev';
+    grunt.registerTask('default', ['uglify:' + target, 'cssmin']);
 };
--- app/config.exceptionHandler.js
+++ app/config.exceptionHandler.js
@@ -1,23 +1,24 @@
 ﻿(function () {
     'use strict';
-    
+
     var app = angular.module('app');
 
     app.config(['$provide', function ($provide) {
         $provide.decorator('$exceptionHandler',
             ['$delegate', 'config', 'logger', extendExceptionHandler]);
     }]);
-    
+
     function extendExceptionHandler($delegate, config, logger) {
         var appErrorPrefix = config.appErrorPrefix;
         var logError = logger.getLogFn('app', 'error');
         return function (exception, cause) {
             $delegate(exception, cause);
-            if (appErrorPrefix && exception.message.indexOf(appErrorPrefix) === 0) { return; }
-
-            var errorData = { exception: exception, cause: cause };
-            var msg = appErrorPrefix + exception.message;
-            logError(msg, errorData, true);
+            throw exception;
+//            if (appErrorPrefix && exception.message.indexOf(appErrorPrefix) === 0) { return; }
+//
+//            var errorData = { exception: exception, cause: cause };
+//            var msg = appErrorPrefix + exception.message;
+//            logError(msg, errorData, true);
         };
     }
 })();
