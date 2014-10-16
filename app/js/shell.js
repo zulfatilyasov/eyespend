77d98d198636853925fa263a1025c48facbf0b23
Zulfat Ilyasov <mail@zulfat.net>
2014-10-17 01:29:07 +0400

    shell coffee

--- app/js/shell.js
+++ app/js/shell.js
@@ -1,87 +1,67 @@
 (function() {
-  var ShellCtrl, controllerId,
+  var Shell,
     __hasProp = {}.hasOwnProperty,
     __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
 
-  this.BaseCtrl = (function() {
-    function BaseCtrl(common) {
+  Shell = (function(_super) {
+    __extends(Shell, _super);
+
+    Shell.register();
+
+    Shell.inject('common', 'debounce', '$rootScope', 'tmhDynamicLocale', 'config', 'login', '$translate', 'cookie');
+
+    function Shell(common, debounce, $rootScope, tmhDynamicLocale, config, login, $translate, cookie) {
+      var events, toggleSpinner;
       this.common = common;
+      this.logout = login.logout;
+      this.langsOpen = false;
+      this.activePage = 'expenses';
+      events = config.events;
+      $rootScope.showSpinner = false;
+      $rootScope.showSpinner = false;
+      $rootScope.lang = config.local;
+      tmhDynamicLocale.set(config.local);
+      $rootScope.closeImageOverlay = function() {
+        $rootScope.showImage = false;
+        return $rootScope.imgUrl = '';
+      };
+      toggleSpinner = function(enable) {
+        return $rootScope.showSpinner = enable;
+      };
+      $rootScope.$on('$routeChangeStart', function(event, next) {
+        if (next.$$route.originalPath === '/') {
+          return toggleSpinner(true);
+        }
+      });
+      $rootScope.$on(events.controllerActivateSuccess, function() {
+        return toggleSpinner(false);
+      });
+      $rootScope.$on(events.spinnerToggle, function(event, data) {
+        return toggleSpinner(true);
+      });
+      this.activate([]).then(function() {
+        return $rootScope.lang = config.local;
+      });
+      this.togglePopover = function() {
+        return this.langsOpen = !this.langsOpen;
+      };
+      this.translate = function(lang) {
+        config.local = lang;
+        cookie.set('lang', lang);
+        $translate.use(lang);
+        if (lang === 'ru') {
+          moment.locale(lang);
+        } else {
+          moment.locale('en-gb');
+        }
+        tmhDynamicLocale.set(lang);
+        $rootScope.lang = lang;
+        return this.togglePopover();
+      };
     }
 
-    BaseCtrl.prototype.activate = function(promises, controllerId) {
-      return this.common.activateController(promises, controllerId).then(function() {
-        return console.log('activated ' + controllerId);
-      });
-    };
+    return Shell;
 
-    BaseCtrl.prototype.register = function(deps, name, module, controller) {
-      return app.module(module).controller(name, deps, controller);
-    };
-
-    return BaseCtrl;
-
-  })();
-
-  controllerId = 'shell';
-
-  angular.module('app').controller(controllerId, [
-    'common', 'debounce', '$rootScope', 'tmhDynamicLocale', 'config', 'login', '$translate', 'cookie', ShellCtrl = (function(_super) {
-      __extends(ShellCtrl, _super);
-
-      function ShellCtrl(common, debounce, $rootScope, tmhDynamicLocale, config, login, $translate, cookie) {
-        var events, toggleSpinner;
-        this.cookie = cookie;
-        ShellCtrl.__super__.constructor.call(this, common);
-        this.logout = login.logout;
-        this.langsOpen = false;
-        this.activePage = 'expenses';
-        events = config.events;
-        $rootScope.showSpinner = false;
-        $rootScope.showSpinner = false;
-        $rootScope.lang = config.local;
-        tmhDynamicLocale.set(config.local);
-        $rootScope.closeImageOverlay = function() {
-          $rootScope.showImage = false;
-          return $rootScope.imgUrl = '';
-        };
-        toggleSpinner = function(enable) {
-          return $rootScope.showSpinner = enable;
-        };
-        $rootScope.$on('$routeChangeStart', function(event, next) {
-          if (next.$$route.originalPath === '/') {
-            return toggleSpinner(true);
-          }
-        });
-        $rootScope.$on(events.controllerActivateSuccess, function() {
-          return toggleSpinner(false);
-        });
-        $rootScope.$on(events.spinnerToggle, function(event, data) {
-          return toggleSpinner(true);
-        });
-        this.activate([], controllerId).then(function() {
-          return $rootScope.lang = config.local;
-        });
-        this.togglePopover = function() {
-          return this.langsOpen = !this.langsOpen;
-        };
-        this.translate = function(lang) {
-          config.local = lang;
-          cookie.set('lang', lang);
-          $translate.use(lang);
-          if (lang === 'ru') {
-            moment.locale(lang);
-          } else {
-            moment.locale('en-gb');
-          }
-          tmhDynamicLocale.set(lang);
-          $rootScope.lang = lang;
-          return this.togglePopover();
-        };
-      }
-
-      return ShellCtrl;
-
-    })(BaseCtrl)
-  ]);
+  })(BaseCtrl);
 
 }).call(this);
--- app/shell/shell.coffee
+++ app/shell/shell.coffee
@@ -1,60 +1,44 @@
-class @BaseCtrl
-    constructor: (@common)->
-    
-    activate: (promises, controllerId)->
-        return @common.activateController(promises, controllerId)
-                        .then -> console.log 'activated ' + controllerId
-    register: (deps, name, module, controller) ->
-        app.module(module).controller(name, deps, controller)
-    
-controllerId = 'shell'
-angular
-    .module 'app'
-    .controller(controllerId, ['common', 'debounce', '$rootScope', 'tmhDynamicLocale', 'config', 'login', '$translate', 'cookie',
-        class ShellCtrl extends BaseCtrl
-            # register(['common', 'debounce', '$rootScope'], controllerId, 'app', @)
-            constructor: (common, debounce, $rootScope, tmhDynamicLocale, config, login, $translate, @cookie) ->
-                super common
-                @logout = login.logout
-                @langsOpen = false
-                @activePage = 'expenses'
-                events = config.events
-                $rootScope.showSpinner = false
-                $rootScope.showSpinner = false
+class Shell extends BaseCtrl
+    @register()
+    @inject 'common', 'debounce', '$rootScope', 'tmhDynamicLocale', 'config', 'login', '$translate', 'cookie'
+    constructor: (@common, debounce, $rootScope, tmhDynamicLocale, config, login, $translate, cookie) ->
+        @logout = login.logout
+        @langsOpen = false
+        @activePage = 'expenses'
+        events = config.events
+        $rootScope.showSpinner = false
+        $rootScope.showSpinner = false
+        $rootScope.lang = config.local
+        tmhDynamicLocale.set config.local
+
+        $rootScope.closeImageOverlay = ->
+            $rootScope.showImage = false 
+            $rootScope.imgUrl = ''
+
+        toggleSpinner = (enable) ->
+            $rootScope.showSpinner = enable
+
+        $rootScope.$on '$routeChangeStart' , (event, next) ->
+            toggleSpinner on if next.$$route.originalPath == '/'
+
+        $rootScope.$on events.controllerActivateSuccess, ->
+            toggleSpinner off
+
+        $rootScope.$on events.spinnerToggle, (event, data) ->
+            toggleSpinner on 
+ 
+        @activate([])
+            .then ->
                 $rootScope.lang = config.local
-                tmhDynamicLocale.set config.local
 
-                $rootScope.closeImageOverlay = ->
-                    $rootScope.showImage = false
-                    $rootScope.imgUrl = ''
+        @togglePopover = ->
+            @langsOpen = !@langsOpen
 
-                toggleSpinner = (enable) ->
-                    $rootScope.showSpinner = enable
-
-                $rootScope.$on '$routeChangeStart' , (event, next) ->
-                    toggleSpinner on if next.$$route.originalPath == '/'
-
-                $rootScope.$on events.controllerActivateSuccess, ->
-                    toggleSpinner off
-
-                $rootScope.$on events.spinnerToggle, (event, data) ->
-                    toggleSpinner on 
-         
-                @activate([], controllerId)
-                    .then ->
-                        $rootScope.lang = config.local
-
-                @togglePopover = ->
-                    @langsOpen = !@langsOpen
-
-                @translate = (lang) ->
-                    config.local = lang
-                    cookie.set 'lang', lang
-                    $translate.use lang
-                    if lang == 'ru' then moment.locale lang else moment.locale 'en-gb'
-                    tmhDynamicLocale.set lang
-                    $rootScope.lang = lang
-                    @togglePopover()
-
-            
-    ])
+        @translate = (lang) ->
+            config.local = lang
+            cookie.set 'lang', lang
+            $translate.use lang
+            if lang == 'ru' then moment.locale lang else moment.locale 'en-gb'
+            tmhDynamicLocale.set lang
+            $rootScope.lang = lang
+            @togglePopover()
